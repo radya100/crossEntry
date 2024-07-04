97189abb1b8492ff0218d3708ed7ab0a3fc60228
goro <goro@manzanagroup.ru>
2024-07-04 17:30:54 +0300

    feat: add buran sh

--- ci/buran.sh
+++ ci/buran.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 cd /etc/bash_etl/crossEntry/ci/
-source ../kim.conn
+source ../buran.conn
 export url="http://$user:$password@$host:8123"
 export rows=200000
-if [ -f ./kim.mli ]; then
-        export mindt=$(<./kim.mli)
+if [ -f ./buran.mli ]; then
+        export mindt=$(<./buran.mli)
 else
         export mindt=$(cat ./initdt.sql | curl $url -sS -d @-)
 fi
@@ -15,7 +15,7 @@ export log_query=$(cat ./log.sql | sed "s/__pb__/$mindt/" | sed "s/__pe__/$maxdt
 export query=$(cat ./insert.sql | sed "s/__pb__/$mindt/" | sed "s/__pe__/$maxdt/")
 export msg=$(echo $query | curl --write-out '%{http_code}' --output /dev/null --silent $url -d @-)
 if [[ "$msg" == "200" ]]; then
-        echo $maxdt>./kim.mli
+        echo $maxdt>./buran.mli
         echo $log_query | curl $url -sS -d @-
 else
         echo "Error!!" 1>&2
