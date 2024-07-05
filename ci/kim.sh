620b8a3c928e2f55cb62ab78a48b89e98083f8c7
goro <goro@manzanagroup.ru>
2024-07-05 18:12:31 +0300

    fix: исправил ошибку когда курсор времени не находит новых записей

--- ci/kim.sh
+++ ci/kim.sh
@@ -9,7 +9,7 @@ if [ -f ./kim.mli ]; then
 else
         export mindt=$(cat ./initdt.sql | curl $url -sS -d @-)
 fi
-export maxdt_query=$(cat ./maxdt.sql | sed "s/__pb__/$mindt/" | sed "s/__rows__/$rows/")
+export maxdt_query=$(cat ./maxdt.sql | sed "s/__pb__/$mindt/g" | sed "s/__rows__/$rows/")
 export maxdt=$(echo $maxdt_query | curl $url -sS -d @-)
 export log_query=$(cat ./log.sql | sed "s/__pb__/$mindt/" | sed "s/__pe__/$maxdt/")
 export query=$(cat ./insert.sql | sed "s/__pb__/$mindt/" | sed "s/__pe__/$maxdt/")
