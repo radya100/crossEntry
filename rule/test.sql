cbf75049fa8fb54db44da1b92c286358086c8d47
goro <goro@manzanagroup.ru>
2024-07-12 13:31:11 +0300

    feat: добавил sh скрипт Кима

--- rule/test.sql
+++ rule/test.sql
@@ -8,4 +8,20 @@ where event_date = today()
     and user = 'airflow_user'
     and type <> 'QueryStart'
     and query like '%rule%'
-order by event_time desc
+order by event_time desc;
+
+
+
+select * from service.rule_del_me where tenant_id = 0;
+
+select event_time
+     , intDiv(query_duration_ms, 60000) as min
+     , formatReadableQuantity(written_rows), query
+from system.query_log
+where event_date >= today()-1
+    and type = 'QueryFinish'
+    and query like 'insert into service.rule_del_me%'
+order by event_time desc;
+
+select * from dwh.rule
+where tenant_id = 0;
