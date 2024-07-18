48bb8f0e3a17572d77935573af9125601c979aa8
goro <goro@manzanagroup.ru>
2024-07-18 15:19:30 +0300

    fix: paid_by_bonus CI fix

--- pcvalue/schema.sql
+++ pcvalue/schema.sql
@@ -52,3 +52,45 @@ create table stage.pcvalue_log on cluster basic
 ) engine = ReplicatedMergeTree('/clickhouse/tables/{shard}/stage_pcvalue_log', '{replica}')
 partition by toYYYYMM(dt_load)
 order by (key_hash);
+
+-->> PCVALUE_CUR
+drop table null.mv_to_stage_pcvalue_from_pcvalue on cluster basic;
+create materialized view null.mv_to_stage_pcvalue_from_pcvalue on cluster basic to stage.pcvalue as
+with 7 as source_table
+    , (toUInt128(10000000000000000000000) * tenant_id)
+        + (toUInt128(100000000000000000000) * source_table)
+        + toUInt128(9223372036854775808)
+        + assumeNotNull(CampaignId) as key_hash
+    , cityHash64(external_id, assumeNotNull(contact_id), assumeNotNull(card_id), assumeNotNull(description), instance_id)
+select
+    key_hash
+--     , instance_id
+    , source_table
+--     , 1 as is_header
+    , is_del
+    , last_version
+
+
+from null.null_pcvalue;
+
+drop table null.mv_to_stage_campaign_keys_from_campaign on cluster basic;
+create materialized view null.mv_to_stage_campaign_keys_from_campaign on cluster basic to stage.campaign_keys as
+with 2 as source_table
+    , (toUInt128(10000000000000000000000) * instance_id)
+        + (toUInt128(100000000000000000000) * source_table)
+        + toUInt128(9223372036854775808)
+        + assumeNotNull(CampaignId) as key_hash
+select
+    key_hash
+    , key_hash as related_hash
+    , cityHash64
+        (
+            assumeNotNull(Name)
+            , assumeNotNull(ActualStart)
+            , assumeNotNull(ActualEnd)
+            , assumeNotNull(externalid)
+            , assumeNotNull(IsActive)
+            , assumeNotNull(OwnerId)
+        ) as attribute_hash
+    , source_table
+from null.loyalty__null__crmdata__Campaign;
