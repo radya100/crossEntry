6f6694557a6ba44de483f2c3d833048d842ea2dc
goro <goro@manzanagroup.ru>
2024-08-02 14:51:07 +0300

    feat: Нужно исправить : 1) cheque_id, chequeitem_id, card_id брать из бонуса а не из чека и позиции

--- bonus/insert.sh
+++ bonus/insert.sh
@@ -3,9 +3,8 @@
 cd /etc/bash_etl/crossEntry/bonus/ || return
 source $1
 export url="http://$user:$password@$host:8123"
-export url2="http://$user:$password@$host:8123?query=insert%20into%20service.qwe%20format%20TSVWithNames"
+export url2="http://$user:$password@$host:8123?query=insert%20into%20service.qwe%20format%20TSVWithNames%20values"
 echo $(<./data_select.sql) | curl $url -sS -d @- | curl $url2 -sS -d @-
-#export rows=2000000
 #if [ -f $state_file ]; then
 #        export mindt=$(<$state_file)
 #else
