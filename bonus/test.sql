bbab825d04e8b086af59ad47c238defeecf835d3
goro <goro@manzanagroup.ru>
2024-08-02 14:58:30 +0300

    feat: Нужно исправить : 1) cheque_id, chequeitem_id, card_id брать из бонуса а не из чека и позиции

--- bonus/test.sql
+++ bonus/test.sql
@@ -198,6 +198,6 @@ select *
 from system.query_log
 where event_date = today()
     and user = 'airflow_user'
-    and type <> 'QueryStart'
+--     and type <> 'QueryStart'
     and query like '%qwe%'
 order by event_time desc
