4f4ab7ace6cb841ac0128f4ec741e9015dc8440d
goro <goro@manzanagroup.ru>
2024-08-28 10:33:23 +0300

    fix: b_instance_hash считается верно

--- bonus/data_select.sql
+++ bonus/data_select.sql
@@ -76,4 +76,4 @@ any left join
     where source_table = 3
     group by chequeitem_id, instance_id
 ) as s4 on s4.chequeitem_id = s1.chequeitem_id and  s4.instance_id = s1.instance_id
--- format Values
+format Values
